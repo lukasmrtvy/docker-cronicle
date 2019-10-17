3b4c2ef51d9dca7cce0dc6a159013ff2fe8cb48f
Lukas M <lukas.mrtvy@gmail.com>
2019-10-17 09:37:41 +0200

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-export CRONICLE_foreground=1
+DATA_DIR=/opt/cronicle/data/
 
-if [ -n "${SETUP_CONFIG+x}" ]; then
-        /opt/cronicle/bin/control.sh  setup
+export CRONICLE_foreground=1
+export CRONICLE_Storage__Filesystem__base_dir=$DATA_DIR
+export CRONICLE_echo=${CRONICLE_echo:-1}
+
+if [ ! "$(ls -A $DATA_DIR)" ]; then
+    echo "$(date -I'seconds') INFO $DATA_DIR is empty, running setup ..."
+    /opt/cronicle/bin/control.sh  setup
+    echo "$(date -I'seconds') INFO done"
 fi
 
 exec "$@"
